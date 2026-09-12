ac52f11ebc162052307ee3caaa81084c072a6e8d
nandikikarunakar <karunakarnandiki31@gmail.com>
2026-09-12 19:38:16 +0530

    added new project

---
+++ 01-band-name-generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to The Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's Your pet name?\n")
+print("The Band Name Suggested is", city + " "+pet)
---
+++ 02-BMI calculator/main.py
@@ -0,0 +1,10 @@
+#BMI CALCULATOR
+#BMI is equal to the person's weight divided by the person's height squared.
+
+print("Calculate Your BMI Here:")
+weight = float(input("Enter Your Weight: "))
+height = float(input("Enter Your Height: "))
+
+bmi = weight/height
+print("Your BMI level is: ", bmi)
+
